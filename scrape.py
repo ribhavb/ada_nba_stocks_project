7ef31f7e08f02ebfca0912c6bbfa802d8af16218
ribhavb <boseribhav@gmail.com>
2023-03-05 20:25:35 -0600

    all data

--- scrape.py
+++ scrape.py
@@ -6,6 +6,6 @@ years = ['2015','2016','2017','2018','2019','2020','2021','2022']
 hustle_stats_2015 = leaguehustlestatsplayer.LeagueHustleStatsPlayer(per_mode_time='PerGame',
     season='2016',season_type_all_star='Regular Season')
 
-print(h for h in hustle_stats_2015)
+print(hustle_stats_2015[:3])
 df = hustle_stats_2015.get_data_frames()[0]
 print(df)
